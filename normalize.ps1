e50d5e1ffafdb0e115f4c890b3a20ac3f951081c
Neutralization <50076677+Neutralization@users.noreply.github.com>
2021-06-03 12:02:08 +0800

    增加Intel Quick Sync

--- normalize.ps1
+++ normalize.ps1
@@ -16,10 +16,12 @@ function Normailze {
     Start-Process -NoNewWindow -Wait -FilePath "ffmpeg.exe" -RedirectStandardError $AudioInfo -ArgumentList $AudioArg
     $AudioData = Get-Content -Path $AudioInfo | Select-Object -Last 12 | ConvertFrom-Json
     $Source = "measured_I=$($AudioData.input_i):measured_LRA=$($AudioData.input_lra):measured_tp=$($AudioData.input_tp):measured_thresh=$($AudioData.input_thresh):offset=$($AudioData.target_offset)"
-    # Nvidia GPU
-    $VideoArg = "-y -hide_banner -loglevel error -ss $($Offset) -t $($Length) -vsync cfr -hwaccel_output_format cuda -c:v h264_cuvid -i .\ranking\list0\$($FileName).mp4 -af $($Target):print_format=summary:linear=true:$($Source) -b:v 20M -c:v h264_nvenc -c:a aac .\ranking\list1\$($FileName).mp4"
+    # Nvidia CUDA
+    $VideoArg = "-y -hide_banner -loglevel error -ss $($Offset) -t $($Length) -vsync cfr -hwaccel_output_format cuda -c:v h264_cuvid -i .\ranking\list0\$($FileName).mp4 -af $($Target):print_format=summary:linear=true:$($Source) -b:v 20M -ar 48000 -c:v h264_nvenc -c:a aac .\ranking\list1\$($FileName).mp4"
+    # Intel Quick Sync
+    # $VideoArg = "-y -hide_banner -loglevel error -ss $($Offset) -t $($Length) -init_hw_device qsv=hw -filter_hw_device hw -hwaccel_output_format qsv -i .\ranking\list0\$($FileName).mp4 -vf hwupload=extra_hw_frames=64,format=qsv -af $($Target):print_format=summary:linear=true:$($Source) -b:v 20M -ar 48000 -c:v h264_qsv -c:a aac .\ranking\list1\$($FileName).mp4"
     # CPU
-    # $VideoArg = "-y -hide_banner -loglevel error -ss $($Offset) -t $($Length) -i .\ranking\list0\$($FileName).mp4 -af $($Target):print_format=summary:linear=true:$($Source) -b:v 20M -c:v libx264 -c:a aac .\ranking\list1\$($FileName).mp4"
+    # $VideoArg = "-y -hide_banner -loglevel error -ss $($Offset) -t $($Length) -i .\ranking\list0\$($FileName).mp4 -af $($Target):print_format=summary:linear=true:$($Source) -b:v 20M -ar 48000 -c:v libx264 -c:a aac .\ranking\list1\$($FileName).mp4"
     Write-Host "$($FileName) Volume Normalizing......"
     Start-Process -NoNewWindow -Wait -FilePath "ffmpeg.exe" -ArgumentList $VideoArg
     Remove-Item $AudioInfo
