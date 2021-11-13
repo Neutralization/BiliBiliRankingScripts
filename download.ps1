f8fe308bc9d9281696a0bf625c88323dd8a49887
Neutralization <50076677+Neutralization@users.noreply.github.com>
2021-11-13 23:14:14 +0800

    使用aria2c下载

--- download.ps1
+++ download.ps1
@@ -71,8 +71,16 @@ function BiliDown {
                 $SourceFiles = $VideoData.data.dash
             }
             try {
-                Invoke-WebRequest -Uri $SourceFiles.audio[0].baseUrl -WebSession $Session -Headers $Headers -OutFile ".\ranking\list0\$($CID)_a.m4s"
-                Invoke-WebRequest -Uri $SourceFiles.video[0].baseUrl -WebSession $Session -Headers $Headers -OutFile ".\ranking\list0\$($CID)_v.m4s"
+                # Invoke-WebRequest -Uri $SourceFiles.audio[0].baseUrl -WebSession $Session -Headers $Headers -OutFile ".\ranking\list0\$($CID)_a.m4s"
+                # Write-Host $SourceFiles.audio[0].baseUrl
+                $aria2cArgs = "-x16 -s12 -j20 -k1M --continue --check-certificate=false --file-allocation=none --summary-interval=0 --download-result=hide --disable-ipv6 ""$($SourceFiles.audio[0].baseUrl)"" --header=""User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0"" --header=""Referer: https://www.bilibili.com"" --out .\ranking\list0\$($CID)_a.m4s"
+                # Write-Host $aria2cArgs
+                Start-Process -NoNewWindow -Wait -FilePath "aria2c.exe" -RedirectStandardError ".\ranking\list0\$($CID)_.log" -ArgumentList $aria2cArgs
+                
+                # Invoke-WebRequest -Uri $SourceFiles.video[0].baseUrl -WebSession $Session -Headers $Headers -OutFile ".\ranking\list0\$($CID)_v.m4s"
+                $aria2cArgs = "-x16 -s12 -j20 -k1M --continue --check-certificate=false --file-allocation=none --summary-interval=0 --download-result=hide --disable-ipv6 ""$($SourceFiles.video[0].baseUrl)"" --header=""User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0"" --header=""Referer: https://www.bilibili.com"" --out .\ranking\list0\$($CID)_v.m4s"
+                # Write-Host $aria2cArgs
+                Start-Process -NoNewWindow -Wait -FilePath "aria2c.exe" -RedirectStandardError ".\ranking\list0\$($CID)_.log" -ArgumentList $aria2cArgs
                 if ($CIDIndex -EQ "0") {
                     $Filename = $BID
                 }
@@ -96,6 +104,7 @@ function BiliDown {
 function Main {
     $RankVideos = @()
     $ThreadNums = (Get-CimInstance Win32_ComputerSystem).NumberOfLogicalProcessors
+    $ThreadNums = 5
     $RankNum = [Math]::Round(((Get-Date).ToFileTime() / 10000000 - 11644473600 - 1277009809) / 3600 / 24 / 7)
 
     Get-ChildItem ".\ranking\list1\$($RankNum)_*.yml" | ForEach-Object {
@@ -121,6 +130,8 @@ function Main {
     $NeedVideos = $RankVideos | Where-Object { $ExistVideos -notcontains $_ }
     $ExtraVideos = $ExistVideos | Where-Object { $RankVideos -notcontains $_ }
     $ExtraVideos | ForEach-Object { Remove-Item ".\ranking\list0\$($_).mp4" }
+
+    # $NeedVideos = @('av633397825')
 
     $Call = $function:BiliDown.ToString()
     $NeedVideos | ForEach-Object -Parallel {
