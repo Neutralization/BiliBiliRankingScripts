a2adbc2670118d36e22a3616f509ae80df802586
Neutralization <50076677+Neutralization@users.noreply.github.com>
2021-12-27 00:19:40 +0800

    改用 HTML 渲染 Unicode 字符

---
+++ unicode.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import json
+from time import sleep
+
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
+
+browser_options = Options()
+browser_options.add_argument("--headless")
+
+browser = Chrome(options=browser_options)
+browser.set_window_size(1920, 1080)
+command = f"/session/{browser.session_id}/chromium/send_command_and_get_result"
+url = browser.command_executor._url + command
+data = json.dumps(
+    {
+        "cmd": "Emulation.setDefaultBackgroundColorOverride",
+        "params": {"color": {"r": 0, "g": 0, "b": 0, "a": 0}},
+    }
+)
+response = browser.command_executor._request("POST", url, data)
+browser.get("D:/Documents/bilibiliweek/unicode.html")
+sleep(1)
+browser.save_screenshot("unicode_text.png")
+browser.quit()
