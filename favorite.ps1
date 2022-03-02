c91c38f756aad3bd8853e6b8f3c9024fdef00ab2
Neutralization <50076677+Neutralization@users.noreply.github.com>
2022-03-02 17:25:59 +0800

    添加收藏稿件

--- favorite.ps1
+++ favorite.ps1
@@ -1,3 +1,6 @@
+param (
+    [string]$RankNum = [Math]::Round(((Get-Date).ToFileTime() / 10000000 - 11644473600 - 1277009809) / 3600 / 24 / 7)
+)
 $ProgressPreference = 'SilentlyContinue'
 
 function AddFavourite {
@@ -6,7 +9,7 @@ function AddFavourite {
         [parameter(position = 2)]$AVID
     )
     $Session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
-    $Cookie = Get-Content -Path "./bilibili.com_cookies.txt"
+    $Cookie = Get-Content -Path "./cookies.txt"
     $CookieString = ""
     $Cookie | ForEach-Object {
         if (!$_.StartsWith('#') -and $_.StartsWith('.bilibili.com')) {
@@ -25,19 +28,36 @@ function AddFavourite {
     $Headers.Add("Referer", "https://www.bilibili.com")
 
     $CSRF = $Session.Cookies.GetCookies("https://www.bilibili.com")["bili_jct"].Value
-    $Params = @{rid = $AVID; type = 2; add_media_ids = $FID; del_media_ids = ""; jsonp = "jsonp"; csrf = $CSRF }
+    $Params = @{rid = $AVID; type = 2; add_media_ids = $FID; del_media_ids = ""; jsonp = "jsonp"; csrf = $CSRF; platform = "web"}
     $Result = (Invoke-WebRequest -Uri "https://api.bilibili.com/x/v3/fav/resource/deal" -Method "POST" -Headers $Headers -Body $Params).Content | ConvertFrom-Json
     Write-Host $Result
 }
 
 $FIDData = @{}
 $FIDList = (Invoke-WebRequest -Uri "https://api.bilibili.com/x/v3/fav/folder/created/list-all?up_mid=398300398&jsonp=jsonp" -Headers $Headers).Content | ConvertFrom-Json
-
 $FIDList.data.list | ForEach-Object {
     $FIDData[$_.title] = $_.id
 }
 
-Get-Content -Path "./MAD_rank1.csv" | ForEach-Object {
-    AddFavourite $FIDData['MAD周刊一位'] $_
-    Start-Sleep -Seconds 2
+$Self = (Invoke-WebRequest -Uri "https://api.bilibili.com/x/space/arc/search?mid=398300398&ps=30&tid=0&pn=1&keyword=$($RankNum)&order=pubdate&jsonp=jsonp" -Headers $Headers).Content | ConvertFrom-Json
+AddFavourite $FIDData['周刊往期合集'] $Self.data.list.vlist[0].aid
+Start-Sleep -Seconds 1
+$Parts = @(16, 3)
+$Files = @()
+$RankVideos = @()
+$Parts | ForEach-Object {
+    $Files += Get-Content -Raw "./ranking/list1/$($RankNum)_$($_).yml"
+}
+$Files | ForEach-Object {
+    ConvertFrom-Yaml $_ | ForEach-Object {
+        $_ | ForEach-Object {
+            $RankVideos += $_. ":name"
+        }
+    }
+}
+AddFavourite $FIDData['周刊历代一位'] $RankVideos[2].Substring(2)
+Start-Sleep -Seconds 1
+$RankVideos[-($RankVideos.Length-3)..-1] | ForEach-Object {
+    AddFavourite $FIDData['周刊往期pickup'] $_.Substring(2)
+    Start-Sleep -Seconds 1
 }
