04b2aa89bc7d2ed24d020f052b9e23bb0024270d
Neutralization <50076677+Neutralization@users.noreply.github.com>
2023-04-28 23:11:20 +0800

    响度标准化

--- normalize.ps1
+++ normalize.ps1
@@ -3,8 +3,12 @@ param (
     [array]$Part = @('*')
 )
 $ProgressPreference = 'SilentlyContinue'
+$TruePath = Split-Path $MyInvocation.MyCommand.Path
+$DownloadFolder = "$($TruePath)/ranking/list0"
+$FootageFolder = "$($TruePath)/ranking/list1"
 
 if (Test-Path -Path 'C:\Windows\System32\nvcuvid.dll') { $Nvdia = $true } else { $Nvdia = $false }
+if ((WMIC CPU Get Name) -match 'Intel') { $Intel = $true } else { $Intel = $false }
 
 function Normailze {
     param (
@@ -12,14 +16,14 @@ function Normailze {
         [parameter(position = 2)]$Offset,
         [parameter(position = 3)]$Length
     )
-    if (-Not(Test-Path "./ranking/list0/$($FileName).mp4")) {
+    if (-Not(Test-Path "$($DownloadFolder)/$($FileName).mp4")) {
         Write-Host "$($FileName).mp4 Not Exist!" -ForegroundColor Red
         return $null
     }
-    $Target = 'loudnorm=I=-12.0:LRA=+7.0:tp=-2.0'
+    $Target = 'loudnorm=I=-23.0:LRA=+7.0:tp=-1.0'
     $Length = $Length + 5
-    $AudioArg = "-y -hide_banner -ss $($Offset) -t $($Length) -i ./ranking/list0/$($FileName).mp4 -af $($Target):print_format=json -f null -"
-    $AudioInfo = "./ranking/list0/$($FileName).log"
+    $AudioArg = "-y -hide_banner -ss $($Offset) -t $($Length) -i $($DownloadFolder)/$($FileName).mp4 -af $($Target):print_format=json -f null -"
+    $AudioInfo = "$($DownloadFolder)/$($FileName).log"
     Start-Process -NoNewWindow -Wait -FilePath 'ffmpeg.exe' -RedirectStandardError $AudioInfo -ArgumentList $AudioArg
     $AudioData = Get-Content -Path $AudioInfo | Select-Object -Last 12 | ConvertFrom-Json
     $Source = "measured_I=$($AudioData.input_i):measured_LRA=$($AudioData.input_lra):measured_tp=$($AudioData.input_tp):measured_thresh=$($AudioData.input_thresh):offset=$($AudioData.target_offset)"
@@ -27,21 +31,52 @@ function Normailze {
     Write-Host "ffmpeg -ss $($Offset) -t $($Length) -i $($FileName).mp4" -ForegroundColor Green
     if ($Nvdia) {
         # Nvidia CUDA
-        $VideoArg = "-y -hide_banner -loglevel error -ss $($Offset) -t $($Length) -vsync cfr "`
-            + "-hwaccel_output_format cuda -c:v h264_cuvid -i ./ranking/list0/$($FileName).mp4 "`
+        $VideoArg = "-y -hide_banner -loglevel error -ss $($Offset) -t $($Length) "`
+            + "-hwaccel_output_format cuda -c:v h264_cuvid "`
+            + "-i $($DownloadFolder)/$($FileName).mp4 "`
             + "-vf scale='ceil((min(1,gt(iw,1920)+gt(ih,1080))*(gte(a,1920/1080)*1920+lt(a,1920/1080)*((1080*iw)/ih))+not(min(1,gt(iw,1920)+gt(ih,1080)))*iw)/2)*2:ceil((min(1,gt(iw,1920)+gt(ih,1080))*(lte(a,1920/1080)*1080+gt(a,1920/1080)*((1920*ih)/iw))+not(min(1,gt(iw,1920)+gt(ih,1080)))*ih)/2)*2' "`
-            + "-af $($Target):print_format=summary:linear=true:$($Source) -b:v 20M -ar 48000 "`
-            + "-c:v h264_nvenc -c:a aac ./ranking/list1/$($FileName).mp4"
+            + "-af $($Target):print_format=summary:linear=true:$($Source) -ar 48000 "`
+            + "-c:v h264_nvenc -b:v 20M -c:a aac -b:a 320k $($FootageFolder)/$($FileName).mp4"
+    }
+    elseif ($Intel) {
+        # Intel QSV
+        $VideoArg = "-y -hide_banner -loglevel error -ss $($Offset) -t $($Length) "`
+            + "-init_hw_device qsv=hw -filter_hw_device hw "`
+            + "-i $($DownloadFolder)/$($FileName).mp4 "`
+            + "-vf hwupload=extra_hw_frames=64,format=qsv "`
+            + "-vf scale='ceil((min(1,gt(iw,1920)+gt(ih,1080))*(gte(a,1920/1080)*1920+lt(a,1920/1080)*((1080*iw)/ih))+not(min(1,gt(iw,1920)+gt(ih,1080)))*iw)/2)*2:ceil((min(1,gt(iw,1920)+gt(ih,1080))*(lte(a,1920/1080)*1080+gt(a,1920/1080)*((1920*ih)/iw))+not(min(1,gt(iw,1920)+gt(ih,1080)))*ih)/2)*2' "`
+            + "-af $($Target):print_format=summary:linear=true:$($Source) -ar 48000 "`
+            + "-c:v h264_qsv -b:v 20M -c:a aac -b:a 320k $($FootageFolder)/$($FileName).mp4"
     }
     else {
-        # CPU
-        $VideoArg = "-y -hide_banner -loglevel error -ss $($Offset) -t $($Length) -i ./ranking/list0/$($FileName).mp4 "`
+        # x264
+        $VideoArg = "-y -hide_banner -loglevel error -ss $($Offset) -t $($Length) "`
+            + "-i $($DownloadFolder)/$($FileName).mp4 "`
             + "-vf scale='ceil((min(1,gt(iw,1920)+gt(ih,1080))*(gte(a,1920/1080)*1920+lt(a,1920/1080)*((1080*iw)/ih))+not(min(1,gt(iw,1920)+gt(ih,1080)))*iw)/2)*2:ceil((min(1,gt(iw,1920)+gt(ih,1080))*(lte(a,1920/1080)*1080+gt(a,1920/1080)*((1920*ih)/iw))+not(min(1,gt(iw,1920)+gt(ih,1080)))*ih)/2)*2' "`
-            + "-af $($Target):print_format=summary:linear=true:$($Source) -b:v 20M -ar 48000 "`
-            + "-c:v libx264 -c:a aac ./ranking/list1/$($FileName).mp4"
+            + "-af $($Target):print_format=summary:linear=true:$($Source) -ar 48000 "`
+            + "-c:v libx264 -b:v 20M -c:a aac -b:a 320k $($FootageFolder)/$($FileName).mp4"
     }
     Start-Process -NoNewWindow -Wait -FilePath 'ffmpeg.exe' -ArgumentList $VideoArg
     Write-Host "$($FileName) Finish!" -ForegroundColor White
+}
+
+
+function EDNormalize {
+    param (
+        [parameter(position = 1)]$FileName
+    )
+    $Target = 'loudnorm=I=-23.0:LRA=+7.0:tp=-1.0'
+    $AudioArg = "-y -hide_banner -i ""./ranking/2_ed/$($FileName).mp3"" -af $($Target):print_format=json -f null -"
+    $AudioInfo = "./ranking/2_ed/ed.log"
+    Start-Process -NoNewWindow -Wait -FilePath 'ffmpeg.exe' -RedirectStandardError $AudioInfo -ArgumentList $AudioArg
+    $AudioData = Get-Content -Path $AudioInfo | Select-Object -Last 12 | ConvertFrom-Json
+    $Source = "measured_I=$($AudioData.input_i):measured_LRA=$($AudioData.input_lra):measured_tp=$($AudioData.input_tp):measured_thresh=$($AudioData.input_thresh):offset=$($AudioData.target_offset)"
+    Write-Host "ffmpeg -i ""./ranking/2_ed/$($FileName).mp3"" "`
+        + "-af 'measured_I=$($AudioData.input_i):LRA=$($AudioData.input_lra):tp=$($AudioData.input_tp)' -> '$($Target)'" -ForegroundColor Blue
+    $VideoArg = "-y -hide_banner -loglevel error -i ""./ranking/2_ed/$($FileName).mp3"" "`
+        + "-af $($Target):print_format=summary:linear=true:$($Source) -ar 48000 "`
+        + "-c:a libmp3lame -q:a 0 ""./ranking/2_ed/ed.mp3"""
+    Start-Process -NoNewWindow -Wait -FilePath 'ffmpeg.exe' -ArgumentList $VideoArg
 }
 
 function Main {
@@ -50,12 +85,12 @@ function Main {
     $LocalVideos = @()
     $RankVideos = @()
     if ($Part.Contains('*')) {
-        $Files = Get-Content -Raw "./ranking/list1/$($RankNum)_*.yml"
-        Get-ChildItem './ranking/list1/*.mp4' | ForEach-Object { $LocalVideos += $_.BaseName }
+        $Files = Get-Content -Raw "$($FootageFolder)/$($RankNum)_*.yml"
+        Get-ChildItem "$($FootageFolder)/*.mp4" | ForEach-Object { $LocalVideos += $_.BaseName }
     }
     else {
         $Part | ForEach-Object {
-            $Files += Get-Content -Raw "./ranking/list1/$($RankNum)_$($_).yml"
+            $Files += Get-Content -Raw "$($FootageFolder)/$($RankNum)_$($_).yml"
         }
     }
     $Files | ForEach-Object {
@@ -70,7 +105,7 @@ function Main {
             if ($LocalVideos -notcontains $_.n) {
                 Normailze $_.n $_.o $_.l
             }
-            elseif ((Get-Item "./ranking/list1/$($_.n).mp4").length -eq 0) {
+            elseif ((Get-Item "$($FootageFolder)/$($_.n).mp4").length -eq 0) {
                 Normailze $_.n $_.o $_.l
             }
             else {
@@ -82,9 +117,15 @@ function Main {
         }
     }
     Add-Type -AssemblyName Microsoft.VisualBasic
-    Get-ChildItem './ranking/list0/*.log' | ForEach-Object {
+    Get-ChildItem "$($DownloadFolder)/*.log" | ForEach-Object {
         [Microsoft.VisualBasic.FileIO.FileSystem]::DeleteFile(
             "$($_)", 'OnlyErrorDialogs', 'SendToRecycleBin')
+    }
+    $EDFile = Get-ChildItem -Path "./ranking/2_ed/*.mp3" | Where-Object BaseName -notmatch 'ed' | Select-Object -ExpandProperty BaseName
+    if ($null -ne $EDFile ) {
+        EDNormalize $EDFile
+        [Microsoft.VisualBasic.FileIO.FileSystem]::DeleteFile(
+            "./ranking/2_ed/$($EDFile).mp3", 'OnlyErrorDialogs', 'SendToRecycleBin')
     }
 }
 
