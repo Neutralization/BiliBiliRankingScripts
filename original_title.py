b2d63d181f58b57501c2960134975aea82c63447
Neutralization <50076677+Neutralization@users.noreply.github.com>
2023-05-23 00:20:02 +0800

    死者苏生

--- original_title.py
+++ original_title.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 
-import asyncio
 import json
 from functools import reduce
 
-import aiohttp
+import requests
 
 from constant import WEEKS
 
 
-async def getVideoTitle(bvid):
-    params = (("bvid", bvid),)
-    async with aiohttp.ClientSession() as session:
-        async with session.get(
-            "https://api.bilibili.com/x/web-interface/view", params=params
-        ) as resp:
-            content = await resp.text()
-            result = json.loads(content)
+def getVideoTitle(bvid):
+    params = {
+        "bvid": bvid,
+    }
+    resp = requests.get(
+        "https://api.bilibili.com/x/web-interface/view",
+        params=params,
+    )
+    result = json.loads(resp.content)
     if result.get("code") == 0:
         title = result["data"]["title"]
         return {bvid: title}
@@ -27,10 +27,7 @@ async def getVideoTitle(bvid):
 def main():
     content = json.load(open(f"./{WEEKS}_results.json", "r", encoding="utf-8"))
     ranks = [rank["bv"] for rank in content if rank.get("bv")]
-    tasks = [asyncio.ensure_future(getVideoTitle(bvid)) for bvid in ranks]
-    loop = asyncio.get_event_loop()
-    VideoTitles = loop.run_until_complete(asyncio.gather(*tasks))
-    VideoTitleDict = reduce(lambda x, y: {**x, **y}, VideoTitles)
+    VideoTitleDict = reduce(lambda x, y: {**x, **y}, map(getVideoTitle, ranks))
     for rank in content:
         if rank.get("bv"):
             if VideoTitleDict[rank.get("bv")] != "":
@@ -40,7 +37,7 @@ def main():
     json.dump(
         content,
         open(f"./{WEEKS}_results.json", "w", encoding="utf-8"),
-        # ensure_ascii=False,
+        ensure_ascii=True,
         indent=4,
     )
 
