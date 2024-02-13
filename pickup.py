63729bee79c2c76570effe4c5fc0de59a1cda846
Neutralization <50076677+Neutralization@users.noreply.github.com>
2024-02-13 23:04:27 +0800

    风控

--- pickup.py
+++ pickup.py
@@ -54,7 +54,18 @@ browser.command_executor._request("POST", browser_url, browser_data)
 
 def GetInfo(aid):
     result = requests.get(
-        f"https://api.bilibili.com/x/web-interface/view?aid={aid}"
+        f"https://api.bilibili.com/x/web-interface/view?aid={aid}",
+        headers={
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+            "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+            "DNT": "1",
+            "Connection": "keep-alive",
+            "Upgrade-Insecure-Requests": "1",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "cross-site",
+        },
     ).json()
     if result["code"] != 0:
         return None
