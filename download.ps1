e8f4aac34e685c5d429f41776204edfe322cf326
Neutralization <50076677+Neutralization@users.noreply.github.com>
2024-02-13 22:23:17 +0800

    还是自己写好

--- download.ps1
+++ download.ps1
@@ -5,151 +5,151 @@ param (
 $ProgressPreference = "SilentlyContinue"
 $TruePath = Split-Path $MyInvocation.MyCommand.Path
 $DownloadFolder = "$($TruePath)/ranking/list0"
-# $CookieFile = "$($TruePath)/bilibili.com_cookies.txt"
-# $DownloadList = "$($TruePath)/download.txt"
-# $UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.26"
-# 
-# $Session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
-# $Session.UserAgent = $UA
-# $CookieString = ""
-# if (Test-Path $CookieFile) {
-#     $Cookies = Get-Content -Path $CookieFile
-#     $Cookies | ForEach-Object {
-#         if (!$_.StartsWith("#") -and $_.StartsWith(".bilibili.com")) {
-#             $Cookie = $_.Split("`t")
-#             $Name = $Cookie[5]
-#             $Value = $Cookie[6]
-#             $Path = $Cookie[2]
-#             $Domain = $Cookie[0]
-#             $CookieString += "$($Name)=$($Value); "
-#             $Session.Cookies.Add((New-Object System.Net.Cookie($Name, $Value, $Path, $Domain)))
-#         }
-#     }
-# }
-# $Headers = @{
-#     "authority"  = "api.bilibili.com"
-#     "method"     = "GET"
-#     "path"       = ""
-#     "scheme"     = "https"
-#     "origin"     = "https://www.bilibili.com"
-#     "referer"    = ""
-#     "user-agent" = $UA
-# }
-# if ("" -ne $CookieString) { $Headers.cookie = $CookieString }
+$CookieFile = "$($TruePath)/bilibili.com_cookies.txt"
+$DownloadList = "$($TruePath)/download.txt"
+$UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.26"
 
-# function ABconvert {
-#     param (
-#         [parameter(position = 1)]$Source,
-#         [parameter(position = 2)]$Target = $true
-#     )
-#     # 如何看待 2020 年 3 月 23 日哔哩哔哩将稿件的「av 号」变更为「BV 号」？ - mcfx的回答 - 知乎
-#     # https://www.zhihu.com/question/381784377/answer/1099438784
-#     $table = "fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF".ToCharArray()
-#     $tr = @{}
-#     0..57 | ForEach-Object {
-#         $tr[$table[$_]] = $_
-#     }
-#     $s = @(11, 10, 3, 8, 4, 6)
-#     $xor = 177451812
-#     $add = 8728348608
-# 
-#     function dec {
-#         param (
-#             [string]$x
-#         )
-#         $r = 0
-#         0..5 | ForEach-Object {
-#             $r += $tr[$x[$s[$_]]] * [Math]::Pow(58, $_)
-#         }
-#         return (($r - $add) -bxor $xor)
-#     }
-#     function enc {
-#         param (
-#             [string]$x
-#         )
-#         $x = ($x -bxor $xor) + $add
-#         $r = "BV1  4 1 7  ".ToCharArray()
-#         0..5 | ForEach-Object {
-#             $r[$s[$_]] = $table[[Math]::Floor($x / [Math]::Pow(58, $_)) % 58]
-#         }
-#         return -join $r
-#     }
-#     if ($Target) {
-#         return dec $Source
-#     } else {
-#         return enc $Source
-#     }
-# }
+$Session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+$Session.UserAgent = $UA
+$CookieString = ""
+if (Test-Path $CookieFile) {
+    $Cookies = Get-Content -Path $CookieFile
+    $Cookies | ForEach-Object {
+        if (!$_.StartsWith("#") -and $_.StartsWith(".bilibili.com")) {
+            $Cookie = $_.Split("`t")
+            $Name = $Cookie[5]
+            $Value = $Cookie[6]
+            $Path = $Cookie[2]
+            $Domain = $Cookie[0]
+            $CookieString += "$($Name)=$($Value); "
+            $Session.Cookies.Add((New-Object System.Net.Cookie($Name, $Value, $Path, $Domain)))
+        }
+    }
+}
+$Headers = @{
+    "authority"  = "api.bilibili.com"
+    "method"     = "GET"
+    "path"       = ""
+    "scheme"     = "https"
+    "origin"     = "https://www.bilibili.com"
+    "referer"    = ""
+    "user-agent" = $UA
+}
+if ("" -ne $CookieString) { $Headers.cookie = $CookieString }
 
-# function BiliDown {
-#     param (
-#         [parameter(position = 1)]$ID,
-#         [parameter(position = 2)]$Part = 1
-#     )
-# 
-#     if ($ID -match "^[aA]") {
-#         $AID = $ID.Substring(2)
-#         $BID = ABconvert $ID.Substring(2) $false
-#     } elseif ($ID -match "^[bB]") {
-#         $AID = ABconvert $ID $true
-#         $BID = $ID
-#     } else {
-#         exit
-#     }
-#     # Write-Output $AID
-#     # Write-Output $BID
-# 
-#     $PageList = "https://api.bilibili.com/x/player/pagelist?aid=$($AID)&jsonp=jsonp"
-#     $Headers.referer = "https://www.bilibili.com/video/av$($AID)/"
-#     $Headers.path = "/x/player/pagelist?aid=$($AID)&jsonp=jsonp"
-#     $Pages = Invoke-WebRequest -UseBasicParsing -Uri $PageList -WebSession $Session -Headers $Headers |
-#         Select-Object -ExpandProperty "Content" | ConvertFrom-Json
-#     $CID = $Pages.data | Where-Object -Property "page" -EQ $Part | Select-Object -ExpandProperty "cid"
-#     # Write-Output $CID
-# 
-#     $CCsub = "https://api.bilibili.com/x/player/v2?aid=$($AID)&cid=$($CID)"
-#     $SubData = Invoke-WebRequest -UseBasicParsing -Uri $CCsub -WebSession $Session -Headers $Headers |
-#         Select-Object -ExpandProperty "Content" | ConvertFrom-Json
-#     if ($null -ne $SubData.data.subtitle.subtitles[0].subtitle_url -and $SubData.data.subtitle.subtitles[0].lan -notmatch 'ai') {
-#         Invoke-WebRequest -Uri "http:$($SubData.data.subtitle.subtitles[0].subtitle_url)" -WebSession $Session -Headers $Headers -OutFile "$($DownloadFolder)/$($CID)_.json"
-#         Start-Process -NoNewWindow -Wait -FilePath "python.exe" -ArgumentList "ccsub2ass.py $($DownloadFolder)/$($CID)_"
-#         $CC = $true
-#     } else {
-#         $CC = $false
-#     }
-# 
-#     $SourceUrl = "https://api.bilibili.com/x/player/playurl?avid=$($AID)&bvid=$($BID)&cid=$($CID)&qn=120&fnver=0&fnval=4048&fourk=1&voice_balance=1"
-#     # Write-Output $SourceUrl
-#     $VideoData = Invoke-WebRequest -UseBasicParsing -Uri $SourceUrl -WebSession $Session -Headers $Headers |
-#         Select-Object -ExpandProperty "Content" | ConvertFrom-Json
-#     Write-Host "$($ID) Video Downloading......"
-# 
-#     $AudioID = $VideoData.data.dash.audio.id | Measure-Object -Maximum | Select-Object -ExpandProperty "Maximum"
-#     $AudioDASH = $VideoData.data.dash.audio | Where-Object -Property "id" -EQ $AudioID |
-#         Select-Object -ExpandProperty "baseUrl"
-#     $VideoID = $VideoData.data.dash.video.id | Measure-Object -Maximum | Select-Object -ExpandProperty "Maximum"
-#     $VideoDASH = $VideoData.data.dash.video | Where-Object -Property "id" -EQ $VideoID |
-#         Where-Object -Property "codecs" -Match "avc" | Select-Object -ExpandProperty "baseUrl"
-#     # Write-Output $AudioDASH
-#     # Write-Output $VideoDASH
-# 
-#     try {
-#         $aria2cArgs = "-x16 -s12 -j20 -k1M --continue --check-certificate=false --file-allocation=none --summary-interval=0 --download-result=hide ""$($AudioDASH)"" --header=""$($UA)"" --header=""Referer: $($Headers.referer)"" --dir=$($DownloadFolder) --out $($CID)_a.m4s"
-#         Start-Process -NoNewWindow -Wait -FilePath "aria2c.exe" -ArgumentList $aria2cArgs -RedirectStandardError "$($DownloadFolder)/$($CID)_.log"
-# 
-#         $aria2cArgs = "-x16 -s12 -j20 -k1M --continue --check-certificate=false --file-allocation=none --summary-interval=0 --download-result=hide ""$($VideoDASH)"" --header=""$($UA)"" --header=""Referer: $($Headers.referer)"" --dir=$($DownloadFolder) --out $($CID)_v.m4s"
-#         Start-Process -NoNewWindow -Wait -FilePath "aria2c.exe" -ArgumentList $aria2cArgs -RedirectStandardError "$($DownloadFolder)/$($CID)_.log"
-# 
-#         if ($CC) {
-#             Write-Output "$($ID) Adding CC subtitles......"
-#             $ffmpegArgs = "-y -hide_banner -i $($DownloadFolder)/$($CID)_a.m4s -i $($DownloadFolder)/$($CID)_v.m4s -c:a copy -vf subtitles=.$($DownloadFolder.Substring($TruePath.Length))/$($CID)_.ass  $($DownloadFolder)/$($ID).mp4"
-#         } else {
-#             $ffmpegArgs = "-y -hide_banner -i $($DownloadFolder)/$($CID)_a.m4s -i $($DownloadFolder)/$($CID)_v.m4s -c copy $($DownloadFolder)/$($ID).mp4"
-#         }
-#         Start-Process -NoNewWindow -Wait -FilePath "ffmpeg.exe" -ArgumentList $ffmpegArgs -RedirectStandardError "$($DownloadFolder)/$($CID)_.log"
-#     } catch { New-Item -Path "$($DownloadFolder)" -Name "$($BID).txt" -ItemType "file" -Value "" -Force }
-#     Remove-Item "$($DownloadFolder)/$($CID)_*.*"
-# }
+function ABconvert {
+    param (
+        [parameter(position = 1)]$Source,
+        [parameter(position = 2)]$Target = $true
+    )
+    # 如何看待 2020 年 3 月 23 日哔哩哔哩将稿件的「av 号」变更为「BV 号」？ - mcfx的回答 - 知乎
+    # https://www.zhihu.com/question/381784377/answer/1099438784
+    $table = "fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF".ToCharArray()
+    $tr = @{}
+    0..57 | ForEach-Object {
+        $tr[$table[$_]] = $_
+    }
+    $s = @(11, 10, 3, 8, 4, 6)
+    $xor = 177451812
+    $add = 8728348608
+
+    function dec {
+        param (
+            [string]$x
+        )
+        $r = 0
+        0..5 | ForEach-Object {
+            $r += $tr[$x[$s[$_]]] * [Math]::Pow(58, $_)
+        }
+        return (($r - $add) -bxor $xor)
+    }
+    function enc {
+        param (
+            [string]$x
+        )
+        $x = ($x -bxor $xor) + $add
+        $r = "BV1  4 1 7  ".ToCharArray()
+        0..5 | ForEach-Object {
+            $r[$s[$_]] = $table[[Math]::Floor($x / [Math]::Pow(58, $_)) % 58]
+        }
+        return -join $r
+    }
+    if ($Target) {
+        return dec $Source
+    } else {
+        return enc $Source
+    }
+}
+
+function BiliDown {
+    param (
+        [parameter(position = 1)]$ID,
+        [parameter(position = 2)]$Part = 1
+    )
+
+    if ($ID -match "^[aA]") {
+        $AID = $ID.Substring(2)
+        $BID = ABconvert $ID.Substring(2) $false
+    } elseif ($ID -match "^[bB]") {
+        $AID = ABconvert $ID $true
+        $BID = $ID
+    } else {
+        exit
+    }
+    # Write-Output $AID
+    # Write-Output $BID
+
+    $PageList = "https://api.bilibili.com/x/player/pagelist?aid=$($AID)&jsonp=jsonp"
+    $Headers.referer = "https://www.bilibili.com/video/av$($AID)/"
+    $Headers.path = "/x/player/pagelist?aid=$($AID)&jsonp=jsonp"
+    $Pages = Invoke-WebRequest -UseBasicParsing -Uri $PageList -WebSession $Session -Headers $Headers |
+        Select-Object -ExpandProperty "Content" | ConvertFrom-Json
+    $CID = $Pages.data | Where-Object -Property "page" -EQ $Part | Select-Object -ExpandProperty "cid"
+    # Write-Output $CID
+
+    $CCsub = "https://api.bilibili.com/x/player/v2?aid=$($AID)&cid=$($CID)"
+    $SubData = Invoke-WebRequest -UseBasicParsing -Uri $CCsub -WebSession $Session -Headers $Headers |
+        Select-Object -ExpandProperty "Content" | ConvertFrom-Json
+    if ($null -ne $SubData.data.subtitle.subtitles[0].subtitle_url -and $SubData.data.subtitle.subtitles[0].lan -notmatch 'ai') {
+        Invoke-WebRequest -Uri "http:$($SubData.data.subtitle.subtitles[0].subtitle_url)" -WebSession $Session -Headers $Headers -OutFile "$($DownloadFolder)/$($CID)_.json"
+        Start-Process -NoNewWindow -Wait -FilePath "python.exe" -ArgumentList "ccsub2ass.py $($DownloadFolder)/$($CID)_"
+        $CC = $true
+    } else {
+        $CC = $false
+    }
+
+    $SourceUrl = "https://api.bilibili.com/x/player/playurl?avid=$($AID)&bvid=$($BID)&cid=$($CID)&qn=120&fnver=0&fnval=4048&fourk=1&voice_balance=1"
+    # Write-Output $SourceUrl
+    $VideoData = Invoke-WebRequest -UseBasicParsing -Uri $SourceUrl -WebSession $Session -Headers $Headers |
+        Select-Object -ExpandProperty "Content" | ConvertFrom-Json
+    Write-Host "$($ID) Video Downloading......"
+
+    $AudioID = $VideoData.data.dash.audio.id | Measure-Object -Maximum | Select-Object -ExpandProperty "Maximum"
+    $AudioDASH = $VideoData.data.dash.audio | Where-Object -Property "id" -EQ $AudioID |
+        Select-Object -ExpandProperty "baseUrl"
+    $VideoID = $VideoData.data.dash.video.id | Measure-Object -Maximum | Select-Object -ExpandProperty "Maximum"
+    $VideoDASH = $VideoData.data.dash.video | Where-Object -Property "id" -EQ $VideoID |
+        Where-Object -Property "codecs" -Match "avc" | Select-Object -ExpandProperty "baseUrl"
+    # Write-Output $AudioDASH
+    # Write-Output $VideoDASH
+
+    try {
+        $aria2cArgs = "-x16 -s12 -j20 -k1M --continue --check-certificate=false --file-allocation=none --summary-interval=0 --download-result=hide ""$($AudioDASH)"" --header=""$($UA)"" --header=""Referer: $($Headers.referer)"" --dir=$($DownloadFolder) --out $($CID)_a.m4s"
+        Start-Process -NoNewWindow -Wait -FilePath "aria2c.exe" -ArgumentList $aria2cArgs -RedirectStandardError "$($DownloadFolder)/$($CID)_.log"
+
+        $aria2cArgs = "-x16 -s12 -j20 -k1M --continue --check-certificate=false --file-allocation=none --summary-interval=0 --download-result=hide ""$($VideoDASH)"" --header=""$($UA)"" --header=""Referer: $($Headers.referer)"" --dir=$($DownloadFolder) --out $($CID)_v.m4s"
+        Start-Process -NoNewWindow -Wait -FilePath "aria2c.exe" -ArgumentList $aria2cArgs -RedirectStandardError "$($DownloadFolder)/$($CID)_.log"
+
+        if ($CC) {
+            Write-Output "$($ID) Adding CC subtitles......"
+            $ffmpegArgs = "-y -hide_banner -i $($DownloadFolder)/$($CID)_a.m4s -i $($DownloadFolder)/$($CID)_v.m4s -c:a copy -vf subtitles=.$($DownloadFolder.Substring($TruePath.Length))/$($CID)_.ass  $($DownloadFolder)/$($ID).mp4"
+        } else {
+            $ffmpegArgs = "-y -hide_banner -i $($DownloadFolder)/$($CID)_a.m4s -i $($DownloadFolder)/$($CID)_v.m4s -c copy $($DownloadFolder)/$($ID).mp4"
+        }
+        Start-Process -NoNewWindow -Wait -FilePath "ffmpeg.exe" -ArgumentList $ffmpegArgs -RedirectStandardError "$($DownloadFolder)/$($CID)_.log"
+    } catch { New-Item -Path "$($DownloadFolder)" -Name "$($BID).txt" -ItemType "file" -Value "" -Force }
+    Remove-Item "$($DownloadFolder)/$($CID)_*.*"
+}
 
 function Main {
     Import-Module powershell-yaml
