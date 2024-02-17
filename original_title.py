e2dca4e7808bd34e548813d45ebf9adbeb84dad8
Neutralization <50076677+Neutralization@users.noreply.github.com>
2024-02-17 13:47:58 +0800

    通过avid获取当前标题

--- original_title.py
+++ original_title.py
@@ -8,16 +8,12 @@ import requests
 from constant import WEEKS
 
 
-def getVideoTitle(bvid):
+def getVideoTitle(aid):
     params = {
-        "bvid": bvid,
+        "aid": aid,
     }
-    if bvid[0] == "a":
-        params = {
-            "aid": bvid[3:],
-        }
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
         "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
         "DNT": "1",
@@ -35,32 +31,14 @@ def getVideoTitle(bvid):
     result = json.loads(resp.content)
     if result.get("code") == 0:
         title = result["data"]["title"]
-        return {bvid: title}
+        return {aid: title}
     else:
         lost = json.load(open("LostFile.json", "r", encoding="utf-8"))
-        lost["name"].append(f"av{bv2av(bvid)}")
+        lost["name"].append(f"av{aid}")
         lost["name"] = list(set(lost["name"]))
         json.dump(lost, open("LostFile.json", "w", encoding="utf-8"))
-        print(f"{bvid}->av{bv2av(bvid)} 404")
-        return {bvid: ""}
-
-
-def bv2av(bvid):
-    table = "fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF"
-    tr = {}
-    for i in range(58):
-        tr[table[i]] = i
-    s = [11, 10, 3, 8, 4, 6]
-    xor = 177451812
-    add = 8728348608
-
-    def dec(x):
-        r = 0
-        for i in range(6):
-            r += tr[x[s[i]]] * 58**i
-        return (r - add) ^ xor
-
-    return dec(bvid)
+        print(f"> av{aid} 啊叻？视频不见了？")
+        return {aid: ""}
 
 
 def main():
@@ -72,12 +50,12 @@ def main():
     ]
     for rankfile in ranklist:
         content = json.load(open(rankfile, "r", encoding="utf-8"))
-        ranks = [rank["bv"] for rank in content if rank.get("bv")]
+        ranks = [rank["wid"] for rank in content if rank.get("wid")]
         VideoTitleDict = reduce(lambda x, y: {**x, **y}, map(getVideoTitle, ranks))
         for rank in content:
-            if rank.get("bv"):
-                if VideoTitleDict[rank.get("bv")] != "":
-                    rank["name"] = VideoTitleDict[rank.get("bv")]
+            if rank.get("wid"):
+                if VideoTitleDict[rank.get("wid")] != "":
+                    rank["name"] = VideoTitleDict[rank.get("wid")]
                 else:
                     pass
         json.dump(
