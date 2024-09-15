72e340f5e5c250504dd15f129a03efdcb833a42c
Neutralization <50076677+Neutralization@users.noreply.github.com>
2024-09-15 22:52:16 +0800

    fix

--- download.ps1
+++ download.ps1
@@ -145,7 +145,7 @@ function BiliDown {
         return
     }
     # 充电专属视频
-    if ($null -ne $VideoData.accept_description.IndexOf('试看')) {
+    if ('-1' -ne $VideoData.accept_description.IndexOf('试看')) {
         Write-Host "$(Get-Date -Format 'MM/dd HH:mm:ss') - 充电专属视频" -ForegroundColor Green
         $VideoMP4 = $VideoData.durl | Where-Object -Property 'order' -EQ 1 | Select-Object -ExpandProperty 'url'
         Write-Debug "$(Get-Date -Format 'MM/dd HH:mm:ss') - 试看视频 $($VideoMP4)"
