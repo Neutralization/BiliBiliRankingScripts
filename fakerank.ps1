77b6f7f259cda753d89c6ef5b7e59df841cddce1
Neutralization <50076677+Neutralization@users.noreply.github.com>
2026-01-26 18:59:20 +0800

    switch to array-based method, adjust parallel logic, change naming style

--- fakerank.ps1
+++ fakerank.ps1
@@ -5,36 +5,44 @@
 )
 $ProgressPreference = 'SilentlyContinue'
 $TruePath = Split-Path $MyInvocation.MyCommand.Path
-$FootageFolder = "$($TruePath)/ranking/list1"
+$FootageFolder = "${TruePath}/ranking/list1"
 
 Import-Module powershell-yaml
-$Files = Get-Content -Raw "$($FootageFolder)/$($RankNum)_*.yml"
-$Files | ForEach-Object {
-    ConvertFrom-Yaml $_ | ForEach-Object {
-        $jobs = @()
-        $_ | ForEach-Object {
-            $rank = $_.':rank'.ToString().PadLeft(2, '0')
-            if (-not (Test-Path -LiteralPath "$($FootageFolder)/$($rank)_$($_.':name').png")) {
-                Copy-Item './footage/MAINRANKIMG.png' "$($FootageFolder)/$($rank)_$($_.':name').png"
-            }
-            if ([int]$_.':length' -ge 30) {
-                if (-not (Test-Path -LiteralPath "$($FootageFolder)/$($rank)_$($_.':name')_.png")) {
-                    Copy-Item './footage/TOPIMG.png' "$($FootageFolder)/$($rank)_$($_.':name')_.png"
-                }
-            }
-            $FakeArg = -join @(
-                "-n -hide_banner -t $($_.':length') -f lavfi -i anullsrc -f lavfi "
-                "-i smptebars=duration=$($_.':length'):size=1280x720:rate=1 "
-                "-vf drawtext=fontfile=C\\:\\\\Windows\\\\Fonts\\\\msyh.ttc:fontsize=100:fontcolor=black:x=(w-text_w)/2:y=(h-text_h)/2:text=$($rank)_$($_.':name') "
-                "$($FootageFolder)/$($rank)_$($_.':name').mp4"
-            )
-            Write-Debug "ffmpeg $($FakeArg)"
-            $jobs += Start-ThreadJob -Name "$($_.':name')" -ArgumentList 'ffmpeg.exe', $FakeArg -ScriptBlock {
-                param($ffmpegExe, $ffmpegArgs)
-                Start-Process -NoNewWindow -Wait -FilePath $ffmpegExe -ArgumentList $ffmpegArgs -RedirectStandardError '.\NUL'
-            }
-        }
-        Wait-Job -Job $jobs
-        Remove-Job -Job $jobs
+$Files = Get-Content -Raw "${FootageFolder}/${RankNum}_*.yml"
+
+$TaskQueue = @()
+foreach ($content in $Files) {
+    $items = (ConvertFrom-Yaml $content) | ForEach-Object { $_ } | ForEach-Object { $_ }
+    $TaskQueue += $items
+}
+
+$CpuCores = $env:NUMBER_OF_PROCESSORS
+$TaskLimit = [Math]::Max(2, [Math]::Floor($CpuCores * 0.75))
+
+$TaskQueue | ForEach-Object -ThrottleLimit $TaskLimit -Parallel {
+    $rank = $_.':rank'.ToString().PadLeft(2, '0')
+    $name = $_.':name'
+    $len = [int]$_.':length'
+    $folder = $using:FootageFolder
+    $fileName = "${rank}_${name}"
+
+    if (-not (Test-Path -LiteralPath "${folder}/${fileName}.png")) {
+        Copy-Item './footage/MAINRANKIMG.png' "${folder}/${fileName}.png"
     }
+    if ($len -ge 30) {
+        if (-not (Test-Path -LiteralPath "${folder}/${fileName}_.png")) {
+            Copy-Item './footage/TOPIMG.png' "${folder}/${fileName}_.png"
+        }
+    }
+
+    $fakeArgs = @(
+        '-n', '-hide_banner',
+        '-t', "${len}",
+        '-f', 'lavfi', '-i', 'anullsrc',
+        '-f', 'lavfi', '-i', "smptebars=duration=${len}:size=1280x720:rate=1",
+        '-vf', "drawtext=fontfile='C\:/Windows/Fonts/msyh.ttc':fontsize=100:fontcolor=black:x=(w-text_w)/2:y=(h-text_h)/2:text='${fileName}'",
+        "${folder}/${fileName}.mp4"
+    )
+
+    & ffmpeg.exe @fakeArgs 2> $null
 }
